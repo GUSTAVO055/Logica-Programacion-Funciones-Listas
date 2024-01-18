b51b58ec12135ee18ff7bdd9e7bea28d641120c3
GUSTAVO ORMEÑO <gustavo.ormeno.055@gmail.com>
2024-01-18 11:07:10 -0300

    Primeros ejercicios

---
+++ desafios1.js
@@ -0,0 +1,23 @@
+//Desafío 1
+alert("Bienvenido a nuestro sitio web.");
+//Desafío 2
+let nombre = "Luna";
+//Desafío 3
+let edad = 25;
+//Desafío 4
+let numeroDeVentas = 50;
+//Desafío 5
+let saldoDisponible = 1000;
+//Desafío 6
+alert("¡Error! Completa todos los campos");
+//Desafío 7
+let mensajeDeError="¡Error! Completa todos los campos";
+alert('Error: ' + mensajeDeError);
+//Desafío 8
+let nombreUsuario=prompt("¿Cuál es su nombre?");
+//Desafío 9
+let edadUsuario = prompt("¿Cuál es su edad?");
+//Desafio 10
+if (edadUsuario>=18) {
+    alert("¡Puedes obtener tu licencia de conducir!")
+}
