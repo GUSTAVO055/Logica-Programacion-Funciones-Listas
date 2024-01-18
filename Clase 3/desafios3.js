75d1fe9f4cd99d588974650f05e586b2de84022a
GUSTAVO ORMEÑO <gustavo.ormeno.055@gmail.com>
2024-01-18 12:23:49 -0300

    Más ejercicios

---
+++ Clase 3/desafios3.js
@@ -0,0 +1,22 @@
+/**
+Desafíos
+
+1-Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+2-Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+3-Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+4-Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+ */
+
+
+//Resoluciones
+//Desafío 1
+let contador=1;
+while (contador<=10) {
+    alert("Contador en: " + contador);
+    contador++;
+}
+
+//Desafío 2
+//Desafío 3
+
+//Desafío 4
