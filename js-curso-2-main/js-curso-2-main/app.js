06a50fb92f5459e2a8abf4b1d2c1147883af6c5a
GUSTAVO ORMEÑO <gustavo.ormeno.055@gmail.com>
2024-01-18 14:54:42 -0300

    Desafío 1

---
+++ js-curso-2-main/js-curso-2-main/app.js
@@ -0,0 +1,9 @@
+let contenido = document.querySelector("h1");
+contenido.innerHTML= "Hora del Desafío";
+
+function consolePresionado(){
+    alert("El botón fue clicado");
+}
+function preguntarCiudad(){
+    alert("El botón fue clicado");
+}
