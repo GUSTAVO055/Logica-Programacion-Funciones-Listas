9b06411d6fb803af0b007d6e5583e31a95b8e467
GUSTAVO ORMEÑO <gustavo.ormeno.055@gmail.com>
2024-01-18 22:33:06 -0300

    Cambios en Clase 4

--- Clase 4/desafios4.js
+++ Clase 4/desafios4.js
@@ -19,86 +19,33 @@ Desafíos
 
 //Resoluciones
 //Desafío 1
-console.log("Bienvenido al sistema.");
+
 
 //Desafío 2
-let nombre = "Gustavo";
-console.log("Hola " + nombre + "!")
+
 
 //Desafío 3
-let nombre3 = "Gustavo";
-alert("Hola " + nombre3 + "!")
+
 
 //Desafío 4
-let respuesta = prompt("¿Cuál es el lenguaje de programación que más te gusta?");
-console.log("Usted ha ingresado: " + respuesta);
+
 
 //Desafío 5
-let valor1d5= 145;
-let valor2d5= 354;
-
-let resultadod5 = valor1d5 + valor2d5;
-
-console.log("La suma de " + valor1d5 + " y " + valor2d5 + " es " + resultado +".");
 
 
 //Desafío 6
-let valor1d6= 145;
-let valor2d6= 354;
-
-let resultadod6 = valor1d6 - valor2d6;
-
-console.log("La resta de " + valor1d6 + " y " + valor2d6 + " es " + resultadod6 +".");
 
 
 //Desafío 7
-let edad = prompt("¿Cuál es su edad?");
 
-if (edad>=18) {
-    console.log("Usted es mayor de edad.");
-}else{
-    console.log("Usted es menor de edad.");
-    
-}
 
 //Desafío 8
-let numerod8=prompt("Ingrese un número:");
-
-if (numerod8==0) {
-    alert("El numero es cero.");
-}else{
-    if(numerod8>0){
-        alert("El numero es positivo.");
-    }else{
-        alert("El numero es negativo.");
-    }
-}
 
 //Desafío 9
-let num=1;
-while (num<=10) {
-    console.log("Número " + num);
-    num++;
-}
+
 
 //Desafío 10
-let nota = prompt("Ingrese nota del alumno:");
 
-if (nota>=7) {
-    console.log("Aprobado");
-}else{
-    console.log("Desaprobado");
-}
 
 //Desafío 11
-let numAleatorio = Math.random();
-console.log("El número aleatorio generado es: " + numAleatorio);
 
-//Desafío 12
-let numAleatoriod12 = Math.random()*10+1;
-console.log("El número aleatorio generado entre 1 y 10 es: " + numAleatoriod12);
-
-
-//Desafío 13
-let numAleatoriod13 = Math.random()*1000+1;
-console.log("El número aleatorio generado entre 1 y 1000 es: " + numAleatoriod13);
